8f9685269bc88ad537e773b6f144fd5b36e2ad2c
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 00:51:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,19 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
     @name = name
   end
   
   def breed=(breed)
     @breed = breed
+  end
+  
+  def name 
+    @name
   end
   
   def breed
