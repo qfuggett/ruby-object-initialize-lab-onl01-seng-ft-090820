fd31a653f9571ec34974ebbbe72394b0fc01ca04
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 00:49:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,13 +7,14 @@ class Dog
     @name = name
   end
   
+   def name 
+    @name
+  end
+  
   def breed=(breed)
     @breed = breed
   end
   
-  def name 
-    @name
-  end
   
   def breed
     @breed
